29b96983c98fc11656ab85c70656145375bac370
Michal Harish <michal.harish@gmail.com>
2019-02-19 20:47:19 +0000

    adapted schema_registry_client to the new fingerprint function

--- schema_registry_client.go
+++ schema_registry_client.go
@@ -59,32 +59,36 @@ func (c *SchemaRegistryClient) GetSchemaId(schema Schema, subject string) uint32
 		c.cache2[subject] = s
 	}
 
-	f := schema.Fingerprint()
-	result, ok := s[f]
-	if !ok {
-		request := make(map[string]string)
-		request["schema"] = schema.String()
-		if schemaJson, err := json.Marshal(request); err != nil {
-			panic(err)
-		} else {
-			log.Printf("Registering schema for subject %q schema: %v", subject, schema.GetName())
-			var url= c.Url + "/subjects/" + subject + "/versions"
-			j := make(map[string]uint32)
-			if resp, err := http.Post(url, "application/json", bytes.NewReader(schemaJson)); err != nil {
-				panic(err)
-			} else if resp.StatusCode != 200 {
-				panic(fmt.Errorf(resp.Status))
-			} else if data, err := ioutil.ReadAll(resp.Body); err != nil {
-				panic(err)
-			} else if err := json.Unmarshal(data, &j); err != nil {
+	if f, err := schema.Fingerprint(); err != nil {
+		panic(err)
+	} else {
+		result, ok := s[*f]
+		if !ok {
+			request := make(map[string]string)
+			request["schema"] = schema.String()
+			if schemaJson, err := json.Marshal(request); err != nil {
 				panic(err)
 			} else {
-				result = j["id"]
-				log.Printf("Got Schema ID: %v", result)
-				s[f] = result
+				log.Printf("Registering schema for subject %q schema: %v", subject, schema.GetName())
+				var url = c.Url + "/subjects/" + subject + "/versions"
+				j := make(map[string]uint32)
+				if resp, err := http.Post(url, "application/json", bytes.NewReader(schemaJson)); err != nil {
+					panic(err)
+				} else if resp.StatusCode != 200 {
+					panic(fmt.Errorf(resp.Status))
+				} else if data, err := ioutil.ReadAll(resp.Body); err != nil {
+					panic(err)
+				} else if err := json.Unmarshal(data, &j); err != nil {
+					panic(err)
+				} else {
+					result = j["id"]
+					log.Printf("Got Schema ID: %v", result)
+					s[*f] = result
+				}
 			}
 		}
+		return result
 	}
-	return result
+
 }
 
